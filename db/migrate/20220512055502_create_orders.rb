841a90ee29f3cc6a9d8cfeef0b68accb74dbd494
sasasasasasaki <howaito220918@yahoo.co.jp>
2022-05-12 13:17:37 +0000

    Add Devise

---
+++ db/migrate/20220512055502_create_orders.rb
@@ -0,0 +1,18 @@
+class CreateOrders < ActiveRecord::Migration[6.1]
+  def change
+    create_table :orders do |t|
+      
+      t.integer :customer_id
+      t.integer :shipping_cost
+      t.integer :total_payment
+      t.integer :payment_method
+      t.integer :status
+      
+      t.string  :postal_code
+      t.string  :address
+      t.string  :name
+
+      t.timestamps
+    end
+  end
+end
