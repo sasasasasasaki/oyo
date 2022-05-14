b0e75a6ad59970425043d29cc568bc5ee2f8c2e5
sasasasasasaki <howaito220918@yahoo.co.jp>
2022-05-14 13:29:30 +0000

    管理者ログイン画面作成途中

---
+++ app/controllers/public/orders_controller.rb
@@ -0,0 +1,13 @@
+class Public::OrdersController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def complete
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,47 @@
 
 Rails.application.routes.draw do
+  scope module: :public do
+  root to: 'homes#top'
+  end
+  
+  namespace :admin do
+    get 'orders/show'
+
+    #get 'customers/index'
+    #get 'customers/edit'
+    #get 'customers/show'
+    resource :customers, only: [:new, :create, :edit, :update, :destroy]
+
+    get 'genres/index'
+    get 'genres/edit'
+
+    get 'items/index'
+    get 'items/new'
+    get 'items/show'
+    get 'items/edit'
+
+    get 'homes/top'
+  end
+  
+  
+  namespace :public do
+    get 'addresses/index'
+    get 'addresses/edit'
+  end
+  namespace :public do
+    get 'orders/index'
+    get 'orders/new'
+    get 'orders/show'
+    get 'orders/complete'
+  end
+  namespace :public do
+    get 'cart_items/index'
+  end
+  namespace :public do
+    get 'customers/show'
+    get 'customers/edit'
+    get 'customers/index'
+  end
   # 顧客用
   devise_for :customers,skip: [:passwords], controllers: {
    registrations: "public/registrations",
