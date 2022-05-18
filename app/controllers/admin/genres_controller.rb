a02a9cddd67b167360883c61da7452df42b2849c
sasasasasasaki <howaito220918@yahoo.co.jp>
2022-05-18 02:20:29 +0000

    ジャンル一覧・追加の画面完了

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -2,19 +2,22 @@ class Admin::GenresController < ApplicationController
 
 
   def create
-      @genre = Genre.new
+      @genre = Genre.new(genre_params)
       @genre.save
       redirect_to admin_genres_path
   end
-    
+
   def index
+    @genre = Genre.new
+    @genres = Genre.all
   end
-  
+
   def edit
+    @genre = Genre.find(params[:id])
   end
-  
+
     private
-    
+
   def genre_params
     params.require(:genre).permit(:name)
   end
